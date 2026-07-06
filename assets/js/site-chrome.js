e5a30b079f6b473b5d7409ca61540424ed50f60a
Helen Ronnenbergh <helenronnenbergh@gmail.com>
2026-07-06 11:41:27 +1000

    Add files via upload

--- assets/js/site-chrome.js
+++ assets/js/site-chrome.js
@@ -197,3 +197,15 @@
     observer.observe(el);
   });
 })();
+
+// Register Service Worker for PWA
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('/sw.js').then(function (registration) {
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function (err) {
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
